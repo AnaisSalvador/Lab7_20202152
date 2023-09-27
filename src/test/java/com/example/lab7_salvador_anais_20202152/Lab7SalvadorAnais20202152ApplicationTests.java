8c18b97043dc74d1a2e5e561ca03cd46212561c8
AnaisSalvador <anaissalvadorrodriguez@gmail.com>
2023-09-27 17:44:22 -0500

    creacion de git

---
+++ src/test/java/com/example/lab7_salvador_anais_20202152/Lab7SalvadorAnais20202152ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab7_salvador_anais_20202152;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7SalvadorAnais20202152ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
